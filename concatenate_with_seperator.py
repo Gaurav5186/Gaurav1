41990bd43f58ada6b8198057b60ef0a886c20930
Gaurav5186 <143017572+Gaurav5186@users.noreply.github.com>
2023-12-01 23:01:58 +0530

    6

---
+++ concatenate_with_seperator.py
@@ -0,0 +1,5 @@
+def concatenate_with_separator(strings, separator):
+    return separator.join(strings)
+s=input("Enter a String")
+s1=input("Enter a Seperator")
+print(concatenate_with_separator(s,s1))
