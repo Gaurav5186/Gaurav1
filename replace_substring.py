7443ace8030b2e2a3001d7369eccc7616a2ef31b
Gaurav5186 <143017572+Gaurav5186@users.noreply.github.com>
2023-12-01 23:18:42 +0530

    9

---
+++ replace_substring.py
@@ -0,0 +1,6 @@
+def replace_substring(original_str, old_substring, new_substring):
+    return original_str.replace(old_substring, new_substring)
+s=input("Enter a string")
+s1=input("Enter a string")
+s2=input("Enter a string")
+print(replace_substring(s,s1,s2))
---
+++ word_count.py
@@ -0,0 +1,4 @@
+def count_words(sentence):
+    return len(sentence.split())
+s=input("Enter a string")
+print(count_words(s))
