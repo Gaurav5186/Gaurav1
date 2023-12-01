8db0442c8ad732426d88eef34d12367002d873ed
Gaurav5186 <143017572+Gaurav5186@users.noreply.github.com>
2023-12-01 23:33:35 +0530

    14

---
+++ count_substring_occurrences.py
@@ -0,0 +1,5 @@
+def count_substring_occurrences(input_str, substring):
+    return input_str.count(substring)
+s=input("Enter a string")
+s1=input("Enter a string")
+print(count_substring_occurrences(s,s1))
