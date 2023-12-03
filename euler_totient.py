baf1cdb318150fe76decfe1a8e0c46665e04ecbe
Gaurav5186 <143017572+Gaurav5186@users.noreply.github.com>
2023-12-03 15:44:50 +0530

    43

---
+++ euler_totient.py
@@ -0,0 +1,15 @@
+def euler_totient_function(n):
+    result = n
+    p = 2
+    while p * p <= n:
+        if n % p == 0:
+            while n % p == 0:
+                n //= p
+            result -= result // p
+        p += 1
+    if n > 1:
+        result -= result // n
+    return result
+
+result = euler_totient_function(36)
+print(result)
