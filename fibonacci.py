b8f3e464612c20ad46d835143225b5cb99acbe58
Gaurav5186 <143017572+Gaurav5186@users.noreply.github.com>
2023-12-03 14:38:31 +0530

    25

---
+++ fibonacci.py
@@ -0,0 +1,14 @@
+def fibonacci(n):
+    fib_sequence = [0, 1]
+    while len(fib_sequence) < n:
+        fib_sequence.append(fib_sequence[-1] + fib_sequence[-2])
+    print(fib_sequence)
+
+fibonacci(10)
+def fibonacci(n):
+    fib_sequence = [0, 1]
+    while len(fib_sequence) < n:
+        fib_sequence.append(fib_sequence[-1] + fib_sequence[-2])
+    print(fib_sequence)
+
+fibonacci(10)
