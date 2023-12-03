747ac2c00623fc00414de25ca9b62625e3951bfe
Gaurav5186 <143017572+Gaurav5186@users.noreply.github.com>
2023-12-03 15:24:59 +0530

    37

---
+++ modular_exponentiation.py
@@ -0,0 +1,12 @@
+def modular_exponentiation(base, exponent, modulus):
+    result = 1
+    base = base % modulus
+    while exponent > 0:
+        if exponent % 2 == 1:
+            result = (result * base) % modulus
+        exponent = exponent // 2
+        base = (base * base) % modulus
+    return result
+
+result = modular_exponentiation(5, 3, 13)
+print(result)
