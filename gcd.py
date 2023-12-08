5f804c6f20f21b2880368bc97e6d7dafeba66801
Gaurav5186 <143017572+Gaurav5186@users.noreply.github.com>
2023-12-08 20:25:48 +0530

    59

--- gcd.py
+++ gcd.py
@@ -1,6 +1,8 @@
-def gcd(a, b):
-    while b:
-        a, b = b, a % b
-    return a
+num1 = 36
+num2 = 60
+gcd = 1
 
-print(gcd(48, 18))
+for i in range(1, min(num1, num2)):
+    if num1 % i == 0 and num2 % i == 0:
+        gcd = i
+print("GCD of", num1, "and", num2, "is", gcd)
